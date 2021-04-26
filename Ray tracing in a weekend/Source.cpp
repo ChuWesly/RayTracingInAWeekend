c4dd21bbf560f7bf6d95a1d458e4c298e863514e
AbsoluteAbsolver <64785006+AbsoluteAbsolver@users.noreply.github.com>
2021-04-26 21:15:27 +0200

    smokey cornel box

--- Ray tracing in a weekend/Source.cpp
+++ Ray tracing in a weekend/Source.cpp
@@ -7,6 +7,8 @@
 #include "moving_sphere.h"
 #include "aarect.h"
 #include "bvh.h"
+#include "box.h"
+#include "constant_medium.h"
 
 #include <iostream>
 #include <fstream>
@@ -126,6 +128,62 @@ hittable_list simple_light() {
     return objects;
 }
 
+hittable_list cornell_box() {
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+    objects.add(box2);
+
+    return objects;
+}
+
+hittable_list cornell_smoke() {
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(7, 7, 7));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+
+    objects.add(make_shared<constant_medium>(box1, 0.01, color(0, 0, 0)));
+    objects.add(make_shared<constant_medium>(box2, 0.01, color(1, 1, 1)));
+
+    return objects;
+}
+
 int main() {
 	// image
     auto aspect_ratio = 16.0 / 9.0;
@@ -143,7 +201,7 @@ int main() {
     auto aperture = 0.0;
     color background(0.70, 0.80, 1.00);
 
-    switch (5) {
+    switch (0) {
     case 1:
         world = bvh_node( random_scene(), 0.0, 1.0 );
         lookfrom = point3(13, 2, 3);
@@ -176,6 +234,26 @@ int main() {
         lookfrom = point3(26, 3, 6);
         lookat = point3(0, 2, 0);
         vfov = 20.0;
+        break;
+    case 6:
+        world = bvh_node(cornell_box(), 0, 1.0);
+        aspect_ratio = 1.0;
+        image_width = 600;
+        samples_per_pixel = 200;
+        background = color(0, 0, 0);
+        lookfrom = point3(278, 278, -800);
+        lookat = point3(278, 278, 0);
+        vfov = 40.0;
+        break;
+    default:
+    case 7:
+        world = bvh_node(cornell_smoke(), 0, 1);
+        aspect_ratio = 1.0;
+        image_width = 600;
+        samples_per_pixel = 20;
+        lookfrom = point3(278, 278, -800);
+        lookat = point3(278, 278, 0);
+        vfov = 40.0;
         break;
     }
 
