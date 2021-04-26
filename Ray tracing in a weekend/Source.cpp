8e85b05b29b59f5075b28c1ec906761a3199a9cf
AbsoluteAbsolver <64785006+AbsoluteAbsolver@users.noreply.github.com>
2021-04-26 17:03:52 +0200

    ''some reorganistion'

--- Ray tracing in a weekend/Source.cpp
+++ Ray tracing in a weekend/Source.cpp
@@ -33,14 +33,14 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
-    return color(0.05, 0.08, 0.09);// (1.0 - t)* color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+    return (1.0 - t)* color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0); //color(0.1, 0.16, 0.18);// 
 }
 
 hittable_list random_scene() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
@@ -82,32 +82,87 @@ hittable_list random_scene() {
     world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
     //auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    auto material3 = make_shared<light_metal>(color(0.7, 0.6, 0.5), 0.9);
+    auto material3 = make_shared<light_metal>(color(0.7, 0.6, 0.5), 0.3);
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     return world;
 }
 
+hittable_list two_spheres() {
+    hittable_list objects;
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+    objects.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    return objects;
+}
+
+hittable_list two_perlin_spheres() {
+    hittable_list objects;
+
+    auto pertext = make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
+
+    return objects;
+}
+
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
+
 int main() {
 	// image
     auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 500;
+    int image_width = 400;
     const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
-    //auto world = random_scene();
-    bvh_node world{ random_scene(), 0.0, 1.0};
+    //bvh_node world{};
+    bvh_node world;
+
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+
+    switch (0) {
+    case 1:
+        world = bvh_node( random_scene(), 0.0, 1.0 );
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        aperture = 0.1;
+        break;
+    case 2:
+        world = bvh_node( two_spheres(), 0.0, 1.0 );
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
+    default:
+    case 3:
+        world = bvh_node( two_perlin_spheres(), 0, 1.0);
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
+    }
 
     // Camera
-    point3 lookfrom(13, 2, 3);
-    point3 lookat(0, 0, 0);
+
     vec3 vup(0, 1, 0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
     int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // io
     const std::string outputfile = "image.ppm";
